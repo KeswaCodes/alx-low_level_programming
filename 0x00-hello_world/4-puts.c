96ac6ed3a8b8edfe1487bf9c1e5977e998eafaed
Zinhle <zinksw@gmail.com>
2023-02-16 14:15:12 -0800

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - The execution point
+*Return - This returns 0
+*/
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+
+  return(0);
+
+}
