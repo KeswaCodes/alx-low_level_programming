37a8c0de1c01c7c91d7dbea9c3fd86fdc83f326a
Zinhle <zinksw@gmail.com>
2023-02-16 16:16:00 -0800

    Intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main-Entry point*
+ *Description: This program prints a set of strings*
+ *Return:Always 0(Success)
+ */
+
+int main(void)
+{
+char[100] sentence="and that piece of art is useful\" - Dora Korpar";
+int date=2015-10-19;
+putchar("%c, %d", sentence, date);
+return(0);
+};
