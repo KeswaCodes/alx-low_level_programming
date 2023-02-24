59065f24f8c0e6cb9dc80879d97dfec77195d9b7
Zinhle <zinksw@gmail.com>
2023-02-23 17:53:20 -0800

    0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,28 +1,23 @@
 #include "main.h"
 
 /**
- * more_numbers - prints more numbers
+ * more_numbers - nmbr fct
+ * Bwave ICT file
+ * Return: no return
  */
 
 void more_numbers(void)
 {
-	int i, j;
 
-	i = 0;
-
-	while (i < 10)
+int i, j;
+	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; j < 15; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-			{
-				_putchar((j / 10) + 48);
-			}
-			_putchar((j % 10) + 48);
+			_putchar('1');
+			_putchar(j % 10 + '0');
 		}
-
-		_putchar('\n');
-
-		i++;
-	}
+			_putchar('\n');
+		}
 }
