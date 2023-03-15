b826570a3e5fa5a6aff5101ebfb44eb0825c9339
Zinhle <zinksw@gmail.com>
2023-03-15 04:38:49 -0700

    Dynamic memory allocation

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include "main.h"
-
 /**
  *_strdup- allocates new space to pointer carrying a copy of *str parameter
  *@str: a pointer to char
@@ -11,6 +10,10 @@ char *_strdup(char *str)
 char *ptr;
 int i;
 int length = 0;
+if (str == NULL)
+{
+return (NULL);
+}
 
 for (i = 0; str[length] != '\0'; i++)
 {
@@ -18,10 +21,7 @@ length++;
 }
 
 ptr = malloc(sizeof(char) * (length + 1));
-if (str == NULL)
-{
-return (NULL);
-}
+
 if (ptr == NULL)
 {
 return (NULL);
