10a0981311c99d9e71fd6bae456c34e5a6cb28c5
Zinhle <zinksw@gmail.com>
2023-03-15 10:27:34 -0700

    Concatenating two strings

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,4 +4,8 @@ char *create_array(unsigned int size, char c);
 int _putchar(int c);
 char *_strdup(char *str);
 char *strcpy(char *dest, const char *src);
+char *str_concat(char *s1, char *s2);
+char *strcat(char *dest, const char *src);
+
 #endif
+
