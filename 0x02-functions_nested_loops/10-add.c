dc933312290abf5a4476e436d8dfcc9fbf52f88b
Zinhle <zinksw@gmail.com>
2023-03-17 22:26:46 -0700

    Wrote a function that adds two integers & returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *add- adds two integer values
+ *Return: the result of the addition
+ */
+
+int add(int a, int b)
+{
+
+int calculateAdd = a + b;
+return (calculateAdd);
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,5 +14,5 @@ void times_table(void);
 void times_table(void);
 void print_to_98(int n);
 void print_times_table(int n);
-
+int add(int, int);
 #endif
