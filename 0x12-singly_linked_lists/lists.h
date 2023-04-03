e60323bcb313ede2c4e4032b318901d199635360
Zinhle <zinksw@gmail.com>
2023-04-02 18:08:20 -0700

    Program returns number of elements in linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -21,4 +21,5 @@ size_t print_list(const list_t *h);
 int putchar(int c);
 size_t strlen(const char *s);
 list_t *add_node(list_t **head, const char *str);
+size_t list_len(const list_t *h);
 #endif
