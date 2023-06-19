f15345b7df19c2848e5c552a982433394d6b174b
Zinhle <zinksw@gmail.com>
2023-06-19 03:59:32 -0700

    Fixes

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,10 +1,10 @@
 /**
- *function- prints the name of the file it was compiled from
- *Return (0) - Success
+ *main- prints the name of the file it was compiled from
+ *Return: (0) - Success
  */
 
 #include <stdio.h>
-int main()
+int main(void)
 {
 printf("%s", __FILE__);
 printf("\n");
