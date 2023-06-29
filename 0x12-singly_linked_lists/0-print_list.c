144347853a63b2f203283fdb9cca46ecbcd9346c
Zinhle <zinksw@gmail.com>
2023-06-29 12:26:32 -0700

    Fixes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,6 @@
 size_t print_list(const list_t *h)
 {
 
-char *string = "[0] (nil)";
 size_t nodes = 0;
 const list_t *ptr;
 
@@ -18,7 +17,7 @@ return (0);
 for (ptr = h; ptr->next != NULL; ptr = ptr->next)
 {
 if (ptr->str == NULL)
-printf("%s\n", string);
+printf("[0] (nil)\n");
 
 else
 printf("[%u] %s\n", ptr->len, ptr->str);
