50f0af8631f57f72f0d476ecd705f467b056f986
Zinhle <zinksw@gmail.com>
2023-06-29 09:54:16 -0700

    Submitting first function

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ *print_list- prints a linked list
+ *@h: pointer to linked list
+ *Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+
+char *string = "[0] (nil)";
+size_t nodes = 0;
+const list_t *ptr;
+
+if (h == NULL)
+return (nodes);
+
+for (ptr = h; ptr->next != NULL; ptr = ptr->next)
+{
+if (ptr->str == NULL)
+printf("%s\n", string);
+else
+printf("[%u] %s\n", ptr->len, ptr->str);
+nodes++;
+}
+printf("[%u] %s\n", ptr->len, ptr->str);
+nodes++;
+
+return (nodes);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,8 @@
+This file contains all my work done in 0x12.C - Singly linked lists.
+
+Files are compiled with gcc, using the options -Wall -Werror -Wextra -pedantic -std=gnu89.
+The only C standard library functions allowed are: malloc, free & exit. The use of _putchar is also allowed.
+Header file lists.h contains all the prototypes of all my functions & the function _putchar.
+The tests folder contains all my test files used to test my code with.
+
+0-print_list - prints all elements of a list_t list.
