05577dfdf976b20b500f8e1d2ca6a9297ebc0c84
Zinhle <zinksw@gmail.com>
2023-06-29 10:23:15 -0700

    Updated function & removed all emacs files

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,18 +13,21 @@ size_t nodes = 0;
 const list_t *ptr;
 
 if (h == NULL)
-return (nodes);
+return (0);
 
 for (ptr = h; ptr->next != NULL; ptr = ptr->next)
 {
 if (ptr->str == NULL)
 printf("%s\n", string);
+
 else
 printf("[%u] %s\n", ptr->len, ptr->str);
 nodes++;
+
 }
 printf("[%u] %s\n", ptr->len, ptr->str);
 nodes++;
+
 
 return (nodes);
 }
