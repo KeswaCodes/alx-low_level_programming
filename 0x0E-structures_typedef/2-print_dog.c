bb49d43547aaa01eb8a05ce3e4970968636fa54f
Zinhle <zinksw@gmail.com>
2023-07-03 12:57:46 -0700

    Fixed print dog function

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,16 +1,19 @@
 #include "dog.h"
-#include <stdio.h>
+
 /**
- *print_dog- prints a struct dog
- *@d: pointer to struct
- *Return: nothing
- */
+  *print_dog- prints a struct dog
+  *@d: pointer to struct
+  *Return: nothing
+  */
 
 void print_dog(struct dog *d)
 {
 
 if (d == NULL)
+{
 printf(" ");
+return;
+}
 
 else
 {
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 #include <stdlib.h>
+#include <stdio.h>
 /**
  *struct dog- a structur e of type struct dog
  *@name: name of the dog
