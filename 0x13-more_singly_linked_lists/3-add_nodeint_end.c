2122d6b4bcdc3a761720d84879672ca6c6f55def
Zinhle <zinksw@gmail.com>
2023-07-04 01:50:02 -0700

    Uploading add_nodeint_end function

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+/**
+ *add_nodeint_end- adds a node at the end of a listint_t list
+ *@head: pointer to head pointer
+ *@n: value to be assigned to node data
+ *Return: pointer to new node, or NULL if it fails
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+listint_t *new_node, *temp;
+
+new_node = malloc(sizeof(listint_t));
+
+if (new_node == NULL)
+return (NULL);
+
+new_node->n = n;
+new_node->next = NULL;
+
+if (*head == NULL)
+{
+*head = new_node;
+return (new_node);
+}
+
+
+temp = *head;
+
+while (temp->next != NULL)
+temp = temp->next;
+
+temp->next = new_node;
+
+return (new_node);
+
+}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -17,3 +17,10 @@ You are allowed to use printf.
 1-listint_len.c :
 Returns the number of elements in a linked listint_t list.
 
+
+2-add_nodeint.c:
+Adds a new node at the beginning of a listint_t list.
+
+
+3-add_nodeint_end.c :
+Adds a new node at the end of a listint_t list.
