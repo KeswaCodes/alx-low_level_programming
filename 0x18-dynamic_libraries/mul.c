be98f7e47ea526898c25303c88334483acb61224
Zinhle <zinksw@gmail.com>
2023-08-21 03:32:08 -0700

    Fixes to script

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,11 @@
+#include "operations.h"
+/**
+ *mul- subtracts 1 integer from another
+ *@a: first parameter
+ *@b: second parameter
+ *Return: the multiplication of the two integers
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
