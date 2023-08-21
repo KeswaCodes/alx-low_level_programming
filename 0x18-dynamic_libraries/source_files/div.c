9babbd5abeba15fd9ddd598c571fc3fb3d94be8a
Zinhle <zinksw@gmail.com>
2023-08-20 23:54:31 -0700

    Calling C functions from Python

---
+++ 0x18-dynamic_libraries/source_files/div.c
@@ -0,0 +1,11 @@
+#include "operations.h"
+/**
+ *div- subtracts 1 integer from another
+ *@a: first parameter
+ *@b: second parameter
+ *Return: the division of the two integers
+ */
+int div(int a, int b)
+{
+return (a / b);
+}
